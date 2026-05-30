da661f23670c8d6d27ef1fd795645adea691b356
biswajit <donot@reply.com>
2026-05-30 20:45:24 +0530

    Update. fetch employee id from env

--- global-setup.js
+++ global-setup.js
@@ -1,7 +1,3 @@
-const { writeFileSync } = require('fs');
-const { join } = require('path');
-
 module.exports = async function globalSetup() {
-  const employeeId = `${Math.floor(Date.now() / 1000)}`;
-  writeFileSync(join(process.cwd(), '.run-state.json'), JSON.stringify({ employeeId }), 'utf8');
+  process.env.EMPLOYEE_ID = `${Math.floor(Date.now() / 1000)}`;
 };
--- tests/fixtures.js
+++ tests/fixtures.js
@@ -1,17 +1,7 @@
 import { test as base, expect } from "@playwright/test";
 import { fixtureEmployee, apiAuth } from "../data/testdata.json";
-import { readFileSync } from "fs";
-import { join } from "path";
 
-let runId;
-try {
-  const state = JSON.parse(readFileSync(join(process.cwd(), ".run-state.json"), "utf8"));
-  runId = state.employeeId;
-} catch {
-  runId = `${Math.floor(Date.now() / 1000)}`;
-}
-
-const runEmployee = { ...fixtureEmployee, id: runId };
+const runEmployee = { ...fixtureEmployee, id: process.env.EMPLOYEE_ID ?? `${Math.floor(Date.now() / 1000)}` };
 
 export const test = base.extend({
   bearerToken: async ({}, use) => {
